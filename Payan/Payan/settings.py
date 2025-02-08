fa93a0a3942fb6969f8b88f0e49971fbb02cbd58
TavakoliMajid <majidtavakoli1997@gmail.com>
2025-02-08 14:50:13 +0100

    Home page improved index.html

--- Payan/Payan/settings.py
+++ Payan/Payan/settings.py
@@ -116,11 +116,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 import os
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "website/static"]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "website/static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
