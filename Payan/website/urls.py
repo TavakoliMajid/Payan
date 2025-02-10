a603bad300b90032bae2cf83cbb44f07adaaed05
amirh147 <amirgrozer@gmail.com>
2025-02-10 23:57:09 +0330

    Change welcome to SHOAR

--- Payan/website/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import home  # ✅ Ensure `home` is the correct function in `views.py`
-
-urlpatterns = [
-    path('', home, name='home'),
-]
--- README.md
+++
@@ -1,2 +0,0 @@
-# Payan
-Payan
